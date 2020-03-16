a4be8d507f90e30cf73fca5f990ba80f4b42e08f
Alaa Ahmed <31044828+AlaaAAmin@users.noreply.github.com>
2020-03-16 13:41:34 +0200

    made a new custom navbar
    made a new custom navbar with grid list
    changed the project font to Roboto

--- octaCourses/src/app/material/material.module.ts
+++ octaCourses/src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
   exports: [
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
