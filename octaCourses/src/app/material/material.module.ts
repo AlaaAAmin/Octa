df25242848bde9c3bd0f766177c72ece6c4df435
Alaa Ahmed <31044828+AlaaAAmin@users.noreply.github.com>
2020-03-17 17:18:24 +0200

    top picks
    added top picks section in the hom page where the recommendation system will display 6 courses to the student

--- octaCourses/src/app/material/material.module.ts
+++ octaCourses/src/app/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   exports: [
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
