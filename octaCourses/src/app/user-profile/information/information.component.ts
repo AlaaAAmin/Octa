963a725d0a1574d6add93a4bc939cc75c59b2642
Alaa Ahmed <31044828+AlaaAAmin@users.noreply.github.com>
2020-03-30 20:22:39 +0200

    changed the login - registeration - profile components
    changelog:
    - made a new design for the registeration components of both the students and courses' providers.
    - mad a new design for the login page
    - made a few changes to the profile page but will be changed later

--- octaCourses/src/app/user-profile/information/information.component.ts
+++ octaCourses/src/app/user-profile/information/information.component.ts
@@ -16,4 +16,67 @@ export class InformationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // the below functions used to display and hide the editing forms for the input fields
+
+  editName:boolean = true;
+  editEmail:boolean = true;
+  editPhone:boolean = true;
+  editFacebookAccount:boolean = true;
+  editBirthdate:boolean = true;
+  changePassword:boolean = true;
+
+  //name section
+  //name edit shows the input field
+  nameEdit(){
+    this.editName = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitName(){
+    this.editName = true;
+  }
+
+  //email section
+  emailEdit(){
+    this.editEmail = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitEmail(){
+    this.editEmail = true;
+  }
+
+  //phone section
+  phoneEdit(){
+    this.editPhone = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitPhone(){
+    this.editPhone = true;
+  }
+
+  //facebook account section
+  facebookAccountEdit(){
+    this.editFacebookAccount = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitFacebookAccount(){
+    this.editFacebookAccount = true;
+  }
+
+  //birthdate section
+  birthdateEdit(){
+    this.editBirthdate = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitBirthdate(){
+    this.editBirthdate = true;
+  }
+
+  //password section
+  passwordEdit(){
+    this.changePassword = false;
+  }
+  // submit name should submit the name to the database then hides the input fields
+  submitPassword(){
+    this.changePassword = true;
+  }
 }
