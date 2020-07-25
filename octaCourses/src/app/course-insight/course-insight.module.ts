3b085829d0617e306eefaa904daa5d7e38cd58bf
Alaa Ahmed <31044828+AlaaAAmin@users.noreply.github.com>
2020-07-25 14:09:57 +0200

    updated the course-insight component
    changelog:
    - added take-quiz component

--- octaCourses/src/app/course-insight/course-insight.module.ts
+++ octaCourses/src/app/course-insight/course-insight.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CourseInsightRoutingModule } from './course-insight-routing.module';
 import { CourseInsightComponent } from './course-insight.component';
 import { MaterialModule } from '../shared/material/material.module';
+import { TakeQuizComponent } from './take-quiz/take-quiz.component';
 
 
 @NgModule({
-  declarations: [CourseInsightComponent],
+  declarations: [CourseInsightComponent, TakeQuizComponent],
   imports: [
     CommonModule,
     CourseInsightRoutingModule,
